279557ac2a013bd50fc022fda88248d12c0a5f04
Felipe <felypebueno2004@gmail.com>
2025-05-23 20:42:51 -0300

    Rodapé fixo na parte superior do site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
         <Header />
-        <main className="container mx-auto p-4">{children}</main>
+        <main className="container mx-auto p-4 flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
